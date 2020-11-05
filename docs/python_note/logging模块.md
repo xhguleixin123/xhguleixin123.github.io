1fa70517b1337fd923b17cd017a954fb572863af
xhguleixin123 <18017158729@163.com>
2020-11-05 15:03:03 +0800

    重写开始

--- docs/python_note/logging模块.md
+++
@@ -1,2 +0,0 @@
-[logging模块博客](https://www.cnblogs.com/Nicholas0707/p/9021672.html)
-
--- docs/python_note/src/log/logtest.py
+++
@@ -1,6 +0,0 @@
-import logging
-logging.debug("debug_msg")
-logging.info("info_msg")
-logging.warning("warning_msg")
-logging.error("error_msg")
-logging.critical("ccritical_msg")
